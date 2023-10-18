8a7166bd198f5cc5e6e79538e1b3af764f2ac0a3
root <root@node2.cluster.local>
2023-10-18 14:20:32 +0900

    최초업로드

---
+++ hellonodejs.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const os = require('os')
+console.log("Test Server Starting...");
+
+var handler = function(request, response)
+{
+        console.log("received request");
+        response.writeHead(200);
+        response.end("Container Hostname: " + os.hostname() + "change jenkins");
+}
+
+ var www = http.createServer(handler);
+ www.listen(8080);
